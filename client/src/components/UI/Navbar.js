cb678fdcab6622941ab45eda842596c5d2626c5b
dmc4719 <dmc4719@gmail.com>
2020-04-01 19:34:47 +0600

    made changes to the ui navbar

--- client/src/components/UI/Navbar.js
+++ client/src/components/UI/Navbar.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './static/Navbar.css'
 import {Link, useHistory} from 'react-router-dom'
 import {logout} from './../../store/actions/authActions'
 import {connect, useDispatch} from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+var flag = false;
+
+
+
 function Navbar(props) {
 const dispatch = useDispatch()
 const history = useHistory()
 const [isClicked, setisClicked] = useState(false)
 
-
-
-   
 
     const onLogout = () => {
       console.log(history)
@@ -21,6 +22,8 @@ const [isClicked, setisClicked] = useState(false)
     const hamMenu = () => {
       setisClicked(!isClicked)
       console.log(isClicked)
+      const nav = document.querySelector('.nav-icon')
+      nav.classList.toggle("ni")
     }
 
     let drawerClasses = '';
@@ -29,9 +32,12 @@ const [isClicked, setisClicked] = useState(false)
         drawerClasses = 'open'
         let elm = document.getElementsByTagName('ul')[0].children
         let i;
+        const nav = document.querySelector('.nav-icon')
         for (i = 0; i < elm.length; i++) {
           elm[i].addEventListener('click', ()=>{
               setisClicked(!isClicked)
+              console.log('clicked')
+              nav.classList.remove("ni")
           })
         }
     }
@@ -43,13 +49,18 @@ const [isClicked, setisClicked] = useState(false)
 
     return (
         <div className="nav">
-            <div className="hamMenu" onClick={hamMenu}style={{display: "none"}}>
+            <div className="mobile">
+            {/* <div className="hamMenu" onClick={hamMenu}style={{display: "none"}}>
                 <div></div>
                 <div></div>
                 <div></div>
-                </div>
+                </div> */}
+                <a className="ham" onClick={hamMenu} style={{display: "none"}} href="#" ><div className="nav-icon"><div></div></div></a>
+                <div className="brand"><Link to="#">Sparta</Link></div>
+            </div>
+          
                 <div>
-                <ul className= {drawerClasses}>
+                <ul className={drawerClasses}>
             
             {
                 props.auth.isAuthenticated?<li style={{cursor:"pointer"}} onClick={onLogout}>Logout</li>:<Link to='/login'><li >Login</li></Link>
